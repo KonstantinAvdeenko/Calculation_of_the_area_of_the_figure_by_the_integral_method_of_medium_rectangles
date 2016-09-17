1f5fe0b8ac2cb6ca8422df32fcae75b52a6d6ec5
KonstantinAvdeenko <AvdeenkoKonstantin1995@gmail.com>
2016-09-17 18:30:25 +0400

    Add my calculation of the area of the figure by the integral method of medium rectangles

---
+++ MetodPryamougolnicov.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetodPryamougolnicov 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//метод средних прямоугольников
+            //h-высота,s-вся площадь,k-точность,n-количество прямоугольников,f-функция
+
+double a, b, h, s, n, k, f;
+
+            Console.WriteLine("Введите нижний предел интеграла");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите верхний предел интеграла");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество прямоугольников");
+            n = double.Parse(Console.ReadLine());
+            h = (b - a) / n; // формула вычисления интеграла методом  средних прямоугольников
+            for (double x1 = 0, x = a; x <= b; x += h)
+            {
+                if (x < b)
+                {
+                    x1 = x + h / 2; // абсолютная погрешность метода средних прямоугольников
+                    if (x1 >= b)
+                        continue;
+                    f = Math.Pow(x1, b) * Math.Sqrt((b * b - x1 * x1));
+                    s += f;
+                }
+            }
+            Console.WriteLine("**********************************");
+            Console.WriteLine("Данный интеграл равен: {0:0.0000}", s * h);
+            Console.ReadKey();
+         }
+     }
+}
---
+++ READ_ME_ENG.md
@@ -0,0 +1,6 @@
+﻿This program is in the C# programming language,
+produces finding the area of ​​a figure using a definite integral using the method of medium rectangles,
+where the limits of a certain integral and the number of rectangles,
+the figure is divided into for more accurate finding of the area, the user enters.
+The main module MetodPryamougolnicov contains a single main function:
+main - fully performing the above task.
